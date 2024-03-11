5c8d06414dc01fe47d35f1b7795fc1aef674f581
Enis Kalyon <fzlnsklyn@gmail.com>
2024-03-11 16:22:40 +0000

    complete shopping cart

---
+++ src/app/cart.tsx
@@ -0,0 +1,24 @@
+import { View, Text, Platform, FlatList } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useCart } from '@/providers/CartProvider';
+import CartListItem from '@/components/CartListItem';
+import Button from '@/components/Button';
+
+const CartScreen = () => {
+    const { items, total } = useCart();
+
+    return (
+        <View style={{ padding: 10 }}>
+            <FlatList 
+            data={items} 
+            contentContainerStyle={{ padding: 10, gap: 10 }}
+            renderItem={({item}) => <CartListItem cartItem={item} />} />
+            <Text style={{ marginTop: 20, fontSize: 20, fontWeight: '500'}}>Total: ${total}</Text>
+            <Button text='Checkout' />
+            <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+          
+        </View>
+    )
+}
+
+export default CartScreen;
